0714163e35dfc330a486c043828d8ade2a71092e
Godreigneth Levi <godreignethlevidibia@yahoo.com>
2021-02-11 18:40:37 +0100

    Created the Home component and added it's hero image

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./index.css";
+
+function index() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src={process.env.PUBLIC_URL + "/images/header/amazon-clone__home-image.jpg"}
+          alt="HomeImage"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default index;
